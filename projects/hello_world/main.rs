86eaee81d9ec84293915cb974a0ce98465b59228
Kashish Lakhara <kashiish19@gmail.com>
2023-08-16 10:47:43 +0530

    hello world

---
+++ projects/hello_world/main.rs
@@ -0,0 +1,9 @@
+/*
+    Define function main.
+    It is always the first code that runs in every executable Rust program.
+    "println!" is called Rust macro. 
+*/
+
+fn main() {
+    println!("Hello world");
+}
