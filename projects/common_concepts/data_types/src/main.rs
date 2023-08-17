2c6cbed1fe6ec8a0dfed89e77598ec9a02970ba5
Kashish Lakhara <kashiish19@gmail.com>
2023-08-17 12:25:27 +0530

    started data-types

---
+++ projects/common_concepts/data_types/src/main.rs
@@ -0,0 +1,14 @@
+/*
+    Rust is statically typed language.
+    The compile usually infer the type based on the value and how we use it. 
+    In cases when many types are possible, we must add a type annotation.
+*/
+
+/*
+    SCALAR TYPES (single value)
+      - Integers, Floating-point numbers, Booleans and Characters.
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
